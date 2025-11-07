2415e51b34393de308b484c0c1d3b0eae4800c47
SkyeVolt <57844971+SkyeVolt@users.noreply.github.com>
2025-11-07 16:34:25 -0500

    ege

---
+++ sketch.js
@@ -0,0 +1,20 @@
+let classifier; 
+let img;
+
+function preload() {
+  classifier = ml5.imageClassifier("MobileNet");
+  img = loadImage("rwr.gif");
+}
+
+function setup() {
+  createCanvas(400, 400);
+  classifier.classify(img, gotResult);
+}
+
+function draw() {
+  background(220);
+}
+
+function gotResult(results) {
+  console.log(results);
+}
